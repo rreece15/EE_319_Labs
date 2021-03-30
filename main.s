9709f68372401c9589eee9b0c0d85a5bf2601643
rreece15 <reece.riherd@utexas.edu>
2021-03-30 12:09:36 -0500

    Update main.s

--- main.s
+++ main.s
@@ -40,41 +40,77 @@ I2C_Send2
 ; 9) wait while I2C is busy, wait for I2C3_MCS_R bit 0 to be 0
 ; 10) return R0 equal to bits 3,2,1 of I2C3_MCS_R, error bits
 ;     will be 0 is no error
-L   LDR R3, =I2C3_MCS_R
+	PUSH {R4, LR}
+L   LDR R3, =I2C3_MCS_R ;step 1-wait for bit 0 to be 0
 	LDR R4, [R3]
 	AND R4, R4, #0x01
 	CMP R4, #0
-	BNE L
-	LDR R3, =I2C3_MSA_R
+	BNE L ;loop for 0
+	LDR R3, =I2C3_MSA_R ;step 2-write slave addres
 	LDR R4, [R3]
 	LSL R0, #1
-	AND R0, R0, #0xFE
-	ORR R4, R4, R0
-	STR R4, [R3]
-	LDR R3, =I2C3_MDR_R
+	AND R0, R0, #0x00FE;clear bit 0, 15-8 of slave address
+	ADD R4, R4, R0
+	STR R4, [R3] ;store slave address
+	LDR R3, =I2C3_MDR_R ;step3
 	LDR R4, [R3]
-	ORR R4, R4, R1
-	STR R4, [R3]
-	LDR R3, =I2C3_MCS_R
+	AND R4, R4, #0xFF00 ;clear first 8 bits of data reg
+	ADD R4, R4, R1 ;put data into R4
+	STR R4, [R3] ;store
+	LDR R3, =I2C3_MCS_R ;step 4
 	MOV R4, #0x03
 	STR R4, [R3]
-	//step 4
-LL  LDR R3, =I2C3_MCS_R
+	NOP
+	NOP
+	NOP
+	NOP
+LL  LDR R3, =I2C3_MCS_R ;step 5
 	LDR R4, [R3]
 	AND R4, R4, #0x01
-	CMP R4, #0
+	CMP R4, #0 ;wait for bit 0 to be 0
 	BNE LL
-	LDR R3, =I2C3_MCS_R
+	LDR R3, =I2C3_MCS_R ;step6
 	LDR R4, [R3]
-	AND R4, R4, 0x0E
-	CMP R4, #2
-	BEQ DIFF
+	AND R4, R4, #0x0E
+	CMP R4, #0 ;if there is no error then continue
+	BEQ con
+	MOV R0, R4 ;R0 = error bits
+	MOV R4, #0x04 ;send stop
+	STR R4, [R3]
+	B dn ;end
 	
+con	LDR R3, =I2C3_MDR_R ;step 7
+	LDR R4, [R3]
+	AND R4, #0xFF00
+	ADD R4, R4, R2
+	STR R4, [R3]
 	
-    BX  LR                          ;   return
+	LDR R3, =I2C3_MCS_R ;step 8
+	LDR R4, [R3]
+	MOV R4, #0x05
+	STR R4, [R3]
+	NOP
+	NOP
+	NOP
+	NOP
+	
+ll	LDR R3, =I2C3_MCS_R ;step 9
+	LDR R4, [R3]
+	AND R4, R4, #0x01
+	CMP R4, #0 ;wait for bit 0 to be 0
+	BNE ll
+	
+	LDR R3, =I2C3_MCS_R ;step 10
+	LDR R4, [R3]
+	AND R4, #0x0E
+	CMP R4, #0
+	BEQ Con
+	B dn
+	
+Con	MOV R0, #0
+	POP {R4, LR}
+dn	BX  LR                          ;   return
 
-DIFF 
 
     ALIGN                           ; make sure the end of this section is aligned
     END                             ; end of file
- 
